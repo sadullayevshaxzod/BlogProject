311bfd1ffe2f118d1e759da9be14f0df69d48b9e
sadullayevshaxzod <shaxzodsadullayev1519@gmail.com>
2026-03-26 11:17:08 +0500

    Bosh sahifaga 'yangi post qo'shish, postni tahrirlash,postni o'chirish' kabi funksiyalar qo'shildi va alohida templates lari ham qo'shildi

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,UpdateView,DeleteView
 from django.views.generic.edit import CreateView
 from .models import Post
+from django.urls import reverse_lazy
 # Create your views here.
 class BlogListView(ListView):
     model=Post
@@ -13,3 +14,11 @@ class BlogCreateView(CreateView):
     model=Post
     template_name='post_new.html'
     fields=['title','author','body']
+class BlogUpdateView(UpdateView):
+    model=Post
+    template_name='post_edit.html'
+    fields=['title','body']
+class BlogDeleteView(DeleteView):
+    model=Post
+    template_name='post_delete.html'
+    success_url=reverse_lazy('home')
